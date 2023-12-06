3fde5650288f1af052beea588396ad02bdb8fdf3
100jinah <winwalking1106@gmail.com>
2023-12-06 12:38:35 +0900

    cors

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Enable CORS for all origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 실제 배포 시에는 허용할 도메인을 명시해주는 것이 좋습니다.
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-class CashFlowInput(BaseModel):
+class ProjectionGraphInput(BaseModel):
     initialPayment: float
     monthlyPayment: float
     MonthlyPaymentPeriod: int
@@ -26,94 +12,92 @@ class CashFlowInput(BaseModel):
     MonthlyAllowancePeriod: int
 
 def find_roots(p):
+    # If input is scalar, this makes it an array
     p = np.atleast_1d(p)
-
+ 
     if p.ndim != 1:
         raise ValueError("Input must be a rank-1 array.")
 
+    # find non-zero array entries
     non_zero = np.nonzero(np.ravel(p))[0]
-
+   
+    # Return an empty array if the polynomial is all zeros
     if len(non_zero) == 0:
         return np.array([])
-
+    
+    # find the number of trailing zeros -- this is the number of roots at 0.
     trailing_zeros = len(p) - non_zero[-1] - 1
+
+    # strip leading and trailing zeros
     p = p[int(non_zero[0]):int(non_zero[-1]) + 1]
 
+    # casting: if incoming array isn't floating point, make it floating point.
     if not issubclass(p.dtype.type, (np.floating, np.complexfloating)):
         p = p.astype(float)
-
+  
     N = len(p)
-
+    print(p)
+    
     if N > 1:
+        # Calculate roots of the polynomial
         roots = np.roots(p)
+        print("Roots:", roots)
     else:
         roots = np.array([])
 
+    # tack any zeros onto the back of the array
     roots = np.hstack((roots, np.zeros(trailing_zeros, dtype=roots.dtype)))
+   
     return roots
 
 def calc_irr(values):
     res = find_roots(values[::-1])
-
+    
+    # Convert complex roots to an array of dictionaries
     roots_result = [{'real': root.real, 'imaginary': root.imag} for root in res]
 
+    # Print roots
+    # print("Roots:", roots_result)
+    
     mask = (res.imag == 0) & (res.real > 0)
 
     if not mask.any():
         return np.nan
-
+    
     res = res[mask].real
     rate = 1 / res - 1
     rate = rate.item(np.argmin(np.abs(rate)))
     return rate
 
-def projection_graph(initialPayment, monthlyPayment, MonthlyPaymentPeriod, monthlyAllowance, MonthlyAllowancePeriod):
+@app.post("/projection_graph")
+def projection_graph(input_data: ProjectionGraphInput):
+    initialPayment = input_data.initialPayment
+    monthlyPayment = input_data.monthlyPayment
+    MonthlyPaymentPeriod = input_data.MonthlyPaymentPeriod
+    monthlyAllowance = input_data.monthlyAllowance
+    MonthlyAllowancePeriod = input_data.MonthlyAllowancePeriod
+
     sharpe_ratio = 1.0
     z = 0.05
     layer = 5
-    cashFlows = [initialPayment]
-
-    for _ in range(MonthlyPaymentPeriod):
+    cashFlows = []
+    cashFlows.append(initialPayment)
+    for i in range(MonthlyPaymentPeriod):
         cashFlows.append(monthlyPayment)
-
-    for _ in range(MonthlyAllowancePeriod):
+    for i in range(MonthlyAllowancePeriod):
         cashFlows.append(-monthlyAllowance)
 
-    irr = calc_irr(cashFlows)
+    # Calculate the IRR
+    irr = calc_irr(cashFlows)  # monthly IRR
     sigma = irr * sharpe_ratio
     irr_yearly = (1.0 + irr) ** (12.0) - 1.0
     print("Internal rate of return:%3.4f" % irr_yearly)
+
     balance = []
-
-    for _ in range(layer):
+    for i in range(layer):
         balance.append(cashFlows.copy())
-
         for j in range(MonthlyPaymentPeriod + MonthlyAllowancePeriod):
-            n = -layer + 2 * _ + 1
-            balance[_][j + 1] = balance[_][j] * (1.0 + irr + n * z * sigma) + balance[_][j + 1]
+            n = -layer + 2 * i + 1
+            balance[i][j + 1] = balance[i][j] * (1.0 + irr + n * z * sigma) + balance[i][j + 1]
 
-    df_balance = pd.DataFrame(data=balance)
-    # df_balance.T.plot()
-
-    # Save the plot to a BytesIO object
-    buffer = BytesIO()
-    df_balance.T.plot(ax=plt.gca())  # Using the existing axis to prevent creating a new figure
-    plt.savefig(buffer, format="png")
-    buffer.seek(0)
-
-    # Encode the image as base64
-    image_base64 = base64.b64encode(buffer.read()).decode("utf-8")
-    plt.close()
-
-    return {"irr_yearly": irr_yearly, "balance": df_balance.to_dict(orient="records"), "plot": image_base64}
-
-@app.post("/calculate-projection")
-async def calculate_projection(cash_flow_input: CashFlowInput):
-    result = projection_graph(
-        cash_flow_input.initialPayment,
-        cash_flow_input.monthlyPayment,
-        cash_flow_input.MonthlyPaymentPeriod,
-        cash_flow_input.monthlyAllowance,
-        cash_flow_input.MonthlyAllowancePeriod,
-    )
-    return result
+    return {"irr_yearly": irr_yearly, "balance": balance}
